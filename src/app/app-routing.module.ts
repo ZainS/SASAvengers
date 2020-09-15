83d7c4177be5a8a4107f00ece5a4ef24cf504380
zasyed <zasyed@ncsu.edu>
2020-09-14 22:49:48 -0400

    more sanity clean up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from "@angular/router";
 import { AvengersListComponent } from './avengers-list/avengers-list.component';
-import { AvengerDetailComponent } from './avenger-detail/avenger-detail.component';
 import { NewAvengerComponent } from './new-avenger/new-avenger.component';
 import { AvengereditComponent } from './avengeredit/avengeredit.component';
 
 const routes: Routes = [
   {path: "home", component: AvengersListComponent},
-  // {path: "detail/:id", component: AvengerDetailComponent},
   {path: "edit/:id", component: AvengereditComponent},
   {path: "newAvenger", component: NewAvengerComponent},
   {path: "", redirectTo: "home", pathMatch: "full"}
--- src/app/avenger-detail/avenger-detail.component.ts
+++ src/app/avenger-detail/avenger-detail.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Avenger } from './../hero/avenger';
 import { AvengerService } from '../hero/avenger.service';
-import { Router, ActivatedRoute } from '@angular/router';
-import { Location } from '@angular/common';
-import { Output, EventEmitter } from '@angular/core';
+
 @Component({
   selector: 'app-avenger-detail',
   templateUrl: './avenger-detail.component.html',
